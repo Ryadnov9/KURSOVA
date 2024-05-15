4455970f32ee7e45be9769fd12dd2e10fa61064c
Roma <stop.ryadnov44@gmail.com>
2024-05-15 13:12:33 +0300

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,37 +26,7 @@ window.addEventListener('load', function() {
 });
 
 
-/*Поиск*/
-function performSearch(query) {
-  var searchResults = document.getElementById('searchResults');
-  var results = [];
-
-  // Очистка результатов предыдущего поиска
-  searchResults.innerHTML = '';
-
-  if (query.trim() !== '') {
-      // Поиск элементов на странице, содержащих запрос
-      var elements = document.querySelectorAll('*:contains("' + query + '")');
-      
-      // Перебор найденных элементов
-      elements.forEach(function(element) {
-          // Создание ссылки для перехода к элементу
-          var link = document.createElement('a');
-          link.textContent = 'Найдено: ' + element.tagName;
-          link.href = '#' + element.id;
-          link.classList.add('searchResultLink');
-
-          // Добавление ссылки к результатам поиска
-          searchResults.appendChild(link);
-
-          // Добавление найденного элемента к результатам поиска (необязательно)
-          searchResults.appendChild(document.createElement('br'));
-      });
-  } else {
-      // В случае пустого запроса можно предпринять соответствующие действия, например, отображение сообщения об ошибке
-      searchResults.textContent = 'Введите текст для поиска';
-  }
-}
+/*Поиск*/ 
 
 
 
